987f4b0d5419efd754bea991d7afc9388539c551
Branislav Hraško <52451064+BraniH@users.noreply.github.com>
2021-10-25 01:34:00 +0200

    Update MailController.py
    MailController - get txt logic upgrade.

--- MailController.py
+++ MailController.py
@@ -2,6 +2,41 @@
 
 import win32com.client as client
 import time
+import os
+
+
+def open_file():
+    all_messages = []
+    with open(os.getcwd() + "\\Mail_texts.txt") as messages:
+        for message_line in messages:
+            all_messages.append(message_line)
+
+        return all_messages
+
+
+def get_text(choice):
+    all_messages = open_file()
+
+    message_body = ""
+    start = 0
+    counter = 0
+    message_status = False
+    for message in all_messages:
+        if choice in message:
+            start = counter
+            message_status = True
+
+        if not message_status:
+            pass
+        elif (counter > start) and ("!!!!!!!END!!!!!!!" == message.replace("\n", "")):
+            message_status = False
+        elif (counter > start) and ("!!!!!!!END!!!!!!!" != message):
+
+            message_body += message
+
+        counter += 1
+
+    return message_body
 
 
 class MailSender:
@@ -13,76 +48,20 @@ class MailSender:
         self.worksheet = worksheet
 
     def mail_settings(self):
-
         if self.worksheet.upper() == "FS":
-            message_body = """
-                <p>Dear SD Team,</p>
-                <p>I&rsquo;m sending you an excel file in the attachment. Please assist us:</p>
-                <p>Please decide on the files on the file server whether the file can be excluded from the PST migration (please delete the file from its location) or whether the file shall be migrated to a user or shared mailbox (if the shared mailbox don&rsquo;t exist, please request the shared mailbox)</p>
-                <p>In case of any technical questions regarding the migrations please, contact <strong>Ettling, Andreas (Heidelberg) DEU.</strong></p>
-                <p><strong>When the check is performed, please send updated excel file to <span style="color: red;"> Andreas Ettling (andreas.ettling@heidelbergcement.com).</span> Do not create ticket in response!</strong></p>
-                <p>Thank you</p>
-                 """
+            message_body = get_text("----File Server----")
+
         elif self.worksheet.upper() == "A":
-            message_body = """
-                <p>Dear SD Team,</p>
-                <p>I&rsquo;m sending you an excel file in the attachment. Please assist us:</p>
-                <p>There is no feedback received from the user. This can be caused by issues between the agent on the client and the migration server. Please restart the agent on the client or restart the client itself or ask the user how to proceed with the PST files detected for him and provide us the feedback and we initiate the migration of the files from the server.</p>
-                <p>To restart the agent please run on the client &ldquo;C:\Program Files (x86)\Quadrotech\Migration Agent\ ResetMigrationAgent.exe&rdquo;</p>
-                <p>In case of any technical questions regarding the migrations please, contact <strong>Ettling, Andreas (Heidelberg) DEU.</strong></p>
-                <p><strong>When the check is performed, please send updated excel file to <span style="color: red;"> Andreas Ettling (andreas.ettling@heidelbergcement.com).</span> Do not create ticket in response!</strong></p>
-                <p>Thank you</p>
-                <p>&nbsp;</p>
-                """
+            message_body = get_text("----Awaiting - Discovered----")
 
         elif self.worksheet.upper() == "O":
-            message_body = """
-                <p>Dear SD Team,</p>
-                <p>I&rsquo;m sending you an excel file in the attachment. Please assist us:</p>
-                <ul>
-                <li>Column status &ldquo;Owner unclear&rdquo;: the tool has not enough information to be sure on the owner &ndash; please contact the users and ask whether they are the owner of the file or the file can be excluded from the migration</li>
-                <li>Column status &ldquo;User is not owner&rdquo;: ask the user about the real owner of the file or whether the file can be excluded from the migration</li>
-                <li>Column status &ldquo;Shared PST&rdquo;: ask the user who shall work with the PST file, check whether a shared mailboxes with proper permissions is existing or request a new mailbox and provide us the name of the shared mailbox or whether the file can be excluded from the migration</li>
-                </ul>
-                <p>In case of any technical questions regarding the migrations please, contact <strong>Ettling, Andreas (Heidelberg) DEU.</strong></p>
-                <p><strong>When the check is performed, please send updated excel file to <span style="color: red;"> Andreas Ettling (andreas.ettling@heidelbergcement.com).</span> Do not create ticket in response!</strong></p>
-                <p>Thank you</p>
-                """
+            message_body = get_text("----Owner unclear - User disagree----")
 
         elif self.worksheet.upper() == "N":
-            message_body = """
-                <p>Dear SD Team,</p>
-                <p>I&rsquo;m sending you an excel file in the attachment. Please assist us:</p>
-                <p>These users are not yet enabled for the PST migration.</p>
-                <ul>
-                <li>Users marked in red are no longer existing, please reply whether the files can be excluded from the PST migration or have to be assigned to a different user</li>
-                <li>Users marked in orange are in the terminated users OU, please reply whether the files can be excluded from the PST migration or have to be assigned to a different user</li>
-                <li>Users marked in green are regarded as completed &ndash; the system hasn&rsquo;t seen the PST files detected for that users for more than 30 days. The files might have been deleted by the user or there is an issue with the agent that the agent is not able to report to the server. Please restart the agent on the client in that case. Please request a confirmation from the user that the PST files are no longer needed or have to be migrated.</li>
-                </ul>
-                <p>In case of any technical questions regarding the migrations please, contact <strong>Ettling, Andreas (Heidelberg) DEU.</strong></p>
-                <p><strong>When the check is performed, please send updated excel file to <span style="color: red;"> Andreas Ettling (andreas.ettling@heidelbergcement.com).</span> Do not create ticket in response!</strong></p>
-                <p>Thank you</p>
-                <p>&nbsp;</p>
-            """
+            message_body = get_text("----Not Enabled Users----")
 
         elif self.worksheet.upper() == "FI":
-            message_body = """
-                <p>p&gt;Dear Team,</p>
-                <p>I&rsquo;m sending you an excel file in the attachment. Please assist us:</p>
-                <p>Not all mails of the PST files could be ingested into the online archive. The sheet is showing the amount of items in the PST file and how many items couldn&rsquo;t be ingested. Please verify with the user whether we can skip the items not migrated.</p>
-                <p>Please verify with the user whether we can skip the items not migrated. The user can verify the details on the items not ingested by himself:</p>
-                <ul>
-                <li>Self service - QUADROtech (heidelbergcement.com)</li>
-                <li>In the column &ldquo;Progress&rdquo; the symbol &ldquo;!&rdquo; indicates files with failed items</li>
-                <li>Click on the symbol in the column &ldquo;Info&rdquo; for this file</li>
-                <li>A popup window opens and at the tab &ldquo;Failed Items Details&rdquo; are all failed items listed</li>
-                </ul>
-                <p>In case there is a very high amount of items which can&rsquo;t be ingested it might be caused by the fact that the Online Archive of the user reached it&rsquo;s limit and after Microsoft has extended the limit of the Online Archive the PST migration tool might be able to migrate further items. In the comment column we added a not which kind of items are missing.</p>
-                <p>In case of any technical questions regarding the migrations please, contact <strong>Ettling, Andreas (Heidelberg) DEU.</strong></p>
-                <p><strong>When the check is performed, please send updated excel file to <span style="color: red;"> Andreas Ettling (andreas.ettling@heidelbergcement.com).</span> Do not create ticket in response!</strong></p>
-                <p>Thank you</p>
-            """
-
+            message_body = get_text("----Failed Items----")
         else:
             print(" ¯\_(ツ)_/¯ Something went wrong, closing!")
             time.sleep(3)
@@ -97,7 +76,7 @@ class MailSender:
         message = outlook.CreateItem(0)
         message.Display()
         message.To = self.receiver
-        #message.CC = "itsupport@heidelbergcement.com"
+        # message.CC = "itsupport@heidelbergcement.com"
         message.SentOnBehalfOfName = "itsupport@heidelbergcement.com"
         subject, body = self.mail_settings()
         message.subject = subject
@@ -110,5 +89,3 @@ class MailSender:
 
 
 # if __name__ == '__main__':
-#     mails = MailSender("xphrask01@vutbr.cz", "112233",
-#                            "C:\\Users\\brani\\Downloads\\ParkedIngestBlockedFilesW49.xlsx")
